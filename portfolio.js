04e6345240d328f022178f2380aa3e206631724e
Jonathanbir <jonathan79111@gmail.com>
2023-01-04 16:48:19 +0800

    commit: 更改連結

--- portfolio.js
+++ portfolio.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
   });
 
   $(".chocolate").click(function (e) {
-    window.open("https://melodic-muffin-e1efeb.netlify.app/");
+    window.open("https://peppy-kitsune-3ce4ff.netlify.app/");
   });
 
   $(".lebledor").click(function (e) {
